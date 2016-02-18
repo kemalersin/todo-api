efeb3926d357f0102d22b3e4b72fc70002ba9bfb
Kemal Ersin Yılmaz <mail@kemalersin.com>
2016-02-18 13:08:35 +0200

    Delete comments

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [];
-var todoNextId = 1;
 
 app.use(bodyParser.json());
 
@@ -37,26 +35,6 @@ app.get('/todos', function (req, res) {
     }, function (e) {
         res.status(500).send();
     });
-
-    //    var filteredTodos = todos;
-    //
-    //    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-    //        filteredTodos = _.where(filteredTodos, {
-    //            completed: true
-    //        });
-    //    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-    //        filteredTodos = _.where(filteredTodos, {
-    //            completed: false
-    //        });
-    //    }
-    //
-    //    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-    //        filteredTodos = _.filter(filteredTodos, function (todo) {
-    //            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-    //        });
-    //    }
-    //
-    //    res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function (req, res) {
@@ -71,16 +49,6 @@ app.get('/todos/:id', function (req, res) {
     }, function (e) {
         res.status(500).send();
     });
-
-    //    var matchedTodo = _.findWhere(todos, {
-    //        id: todoId
-    //    });
-    //
-    //    if (matchedTodo) {
-    //        res.json(matchedTodo);
-    //    } else {
-    //        res.status(404).send();
-    //    }
 });
 
 app.post('/todos', function (req, res) {
@@ -91,16 +59,6 @@ app.post('/todos', function (req, res) {
     }, function (e) {
         res.status(400).json(e);
     });
-
-    //    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    //        return res.status(400).send();
-    //    }
-    //
-    //    body.id = todoNextId++;
-    //    body.description = body.description.trim();
-    //
-    //    todos.push(body);
-    //    res.json(body);
 });
 
 app.delete('/todos/:id', function (req, res) {
@@ -121,19 +79,6 @@ app.delete('/todos/:id', function (req, res) {
     }, function (e) {
         res.status(500).send();
     });
-
-    //    var matchedTodo = _.findWhere(todos, {
-    //        id: todoId
-    //    });
-    //
-    //    if (!matchedTodo) {
-    //        res.status(404).json({
-    //            "error": "no todo found with that id"
-    //        });
-    //    } else {
-    //        todos = _.without(todos, matchedTodo);
-    //        res.json(matchedTodo);
-    //    }
 });
 
 app.put('/todos/:id', function (req, res) {
@@ -162,32 +107,6 @@ app.put('/todos/:id', function (req, res) {
     }, function (e) {
         res.status(500).send();
     });
-
-    //    var matchedTodo = _.findWhere(todos, {
-    //        id: todoId
-    //    });
-    //    var body = _.pick(req.body, 'description', 'completed');
-    //    var validAttributes = {};
-    //
-    //    if (!matchedTodo) {
-    //        return res.status(404).send();
-    //    }
-    //
-    //    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-    //        validAttributes.completed = body.completed;
-    //    } else if (body.hasOwnProperty('completed')) {
-    //        return res.status(400).send();
-    //    }
-    //
-    //    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-    //        validAttributes.description = body.description;
-    //    } else if (body.hasOwnProperty('description')) {
-    //        return res.status(400).send();
-    //    }
-    //
-    //    _.extend(matchedTodo, validAttributes);
-    //
-    //    res.json(matchedTodo);
 });
 
 db.sequelize.sync().then(function () {
