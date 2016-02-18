bcdccb5ef5de6e9d9923df8c1741ab8f59e893bc
Kemal Ersin Yılmaz <mail@kemalersin.com>
2016-02-18 16:00:18 +0200

    Add login route and authenticate class method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -116,6 +117,16 @@ app.put('/todos/:id', function (req, res) {
         }
     }, function (e) {
         res.status(500).send();
+    });
+});
+
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    }, function (e) {
+        res.status(401).send();
     });
 });
 
