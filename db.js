77b548748e2fad4b2153b56faad4aab48c49c344
Kemal Ersin Yılmaz <mail@kemalersin.com>
2016-02-19 10:51:58 +0200

    Adding todo/user association

--- db.js
+++ db.js
@@ -13,16 +13,14 @@ if (env === 'production') {
     });
 }
 
-//var sequelize = new Sequelize(undefined, undefined, undefined, {
-//    'dialect': 'sqlite',
-//    'storage': __dirname + '/data/dev-todo-api.sqlite'
-//});
-
 var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.user = sequelize.import(__dirname + '/models/user.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.todo.belongsTo(db.user);
+db.user.hasMany(db.todo);
 
 module.exports = db;
